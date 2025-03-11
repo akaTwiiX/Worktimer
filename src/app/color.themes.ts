07c966eeb9e23b79a130f645fafcd82f1b7d8127
John Fieweger <john.fieweger98@gmail.com>
2025-03-11 12:25:44 +0100

    style: add themes

---
+++ src/app/color.themes.ts
@@ -0,0 +1,8 @@
+export const Colors = [
+    { value: '#f7d186', label: 'YELLOW' },
+    { value: '#95d0f0', label: 'BLUE' },
+    { value: '#91c98b', label: 'GREEN' },
+    { value: '#a09fe3', label: 'PURPLE' },
+    { value: '#f79a86', label: 'RED' },
+
+]
