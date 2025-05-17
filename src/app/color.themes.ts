319c2394f01af6035e9a174ad6039babc791271e
akaTwiiX <104139228+akaTwiiX@users.noreply.github.com>
2025-05-17 11:17:26 +0200

    style: change color themes

--- src/app/color.themes.ts
+++ src/app/color.themes.ts
@@ -1,8 +1,8 @@
 export const Colors = [
     { value: '#f7d186', label: 'YELLOW' },
     { value: '#95d0f0', label: 'BLUE' },
-    { value: '#91c98b', label: 'GREEN' },
+    { value: '#ffffcc', label: 'GREEN' },
     { value: '#a09fe3', label: 'PURPLE' },
     { value: '#f79a86', label: 'RED' },
-    { value: '#f2b8ea', label: 'LIGHT PINK' }
+    { value: '#ffccbb', label: 'LIGHT PINK' }
 ]
