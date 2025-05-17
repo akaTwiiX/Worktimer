bee66da779b8339491b381a73d8165be6f834124
akaTwiiX <104139228+akaTwiiX@users.noreply.github.com>
2025-05-17 11:22:15 +0200

    style: change theme color

--- src/app/color.themes.ts
+++ src/app/color.themes.ts
@@ -1,7 +1,7 @@
 export const Colors = [
     { value: '#f7d186', label: 'YELLOW' },
     { value: '#95d0f0', label: 'BLUE' },
-    { value: '#ffffcc', label: 'GREEN' },
+    { value: '#b9e0ab', label: 'GREEN' },
     { value: '#a09fe3', label: 'PURPLE' },
     { value: '#f79a86', label: 'RED' },
     { value: '#ffccbb', label: 'LIGHT PINK' }
